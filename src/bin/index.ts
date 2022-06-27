edae72a3ebaa2fe4940f9339c187ed7b5ba07433
Andrius Mozūraitis <andriusmozuraitis@gmail.com>
2022-06-27 12:08:54 +0300

    Improve something

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -1,22 +1,52 @@
 import * as Jet from 'fs-jetpack'
-import * as Path from 'path'
+import path, * as Path from 'path'
 import * as P from 'ts-prime'
 import chokidar from 'chokidar'
 import liveServer from 'live-server'
-import renderToString from 'preact-render-to-string'
-import { generatePdfBase } from '../generatePDFContent'
+import { generatePdfBase } from '@barbora-express/pdf-components'
 
-export async function watchFile(file: string) {
-	const tmpFolder = `/tmp/${P.uuidv4()}/`
-	await Jet.dir(tmpFolder)
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
+import { register } from 'ts-node'
+register()
+
+function packageJson() {
+	let path = 'package.json'
+	for (const index of P.range(1, 50)) {
+		const r = Jet.exists(path)
+		if (r) {
+			return path
+		}
+		const f = P.range(0, index)
+			.map(() => '../')
+			.join('')
+
+		path = Path.resolve(f, 'package.json')
+	}
+	throw new Error('Failed to find package.json')
+}
+export function root() {
+	return path.dirname(packageJson())
 }
 
-export async function servePDF(file: string) {
+export async function servePDF() {
+	const args = await yargs(hideBin(process.argv))
+		.option('file', {
+			alias: 'v',
+			type: 'string',
+		})
+		.parse(process.argv)
+	if (args.file == null) {
+		console.log('Please provide --file argument')
+		process.exit(1)
+	}
+	const file = path.resolve(args.file)
 	process.env.LIVE_SERVER = 'true'
 	const tmpFolder = `/tmp/${P.uuidv4()}/`
 	await Jet.dir(tmpFolder)
 	const run = async () => {
-		const files = Object.keys(require.cache).filter((q) => q.startsWith(Path.dirname(Path.resolve(file))))
+		const files = Object.keys(require.cache).filter((q) => !q.includes('node_modules'))
+		console.log(files)
 		for (const x of files) {
 			delete require.cache[x]
 		}
@@ -24,7 +54,7 @@ export async function servePDF(file: string) {
 		await Jet.writeAsync(Path.resolve(tmpFolder, 'index.html'), generatePdfBase(component.default))
 	}
 	const debounced = P.debounce(run, 100)
-	const watcher = chokidar.watch(Path.dirname(file), {
+	const watcher = chokidar.watch(Path.resolve(root(), 'src'), {
 		ignored: /^\./,
 		persistent: true,
 	})
@@ -38,5 +68,5 @@ export async function servePDF(file: string) {
 		file: 'index.html', // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
 	})
 }
-console.log('as')
-servePDF(Path.resolve(__dirname, './sample.tsx'))
+
+servePDF()
--- src/pdf.ts
+++ src/pdf.ts
@@ -2,85 +2,58 @@ import * as P from 'ts-prime'
 import * as Jet from 'fs-jetpack'
 import { tmpNameSync } from 'tmp'
 import { PDFDocument } from 'pdf-lib'
-import { flipDimensions } from './utils'
-import { VNode } from 'preact'
-import renderToString from 'preact-render-to-string'
+import { Content, flipDimensions } from '@barbora-express/pdf-components'
+
 const html_to_pdf = require('html-pdf-node')
 
-export namespace PDF {
-	export type Format = 'A3' | 'A4' | 'A5' | 'A6' | [x: number, y: number]
+async function mergePDF(pdfList: Buffer[]) {
+	const pdfToMerge = [...pdfList]
 
-	export interface Content {
-		content: string
-		format: Format
-		landscape?: boolean
+	const mergedPdf = await PDFDocument.create()
+	for (const pdfBytes of pdfToMerge) {
+		const pdf = await PDFDocument.load(pdfBytes)
+		const copiedPages = await mergedPdf.copyPages(pdf, pdf.getPageIndices())
+		copiedPages.forEach((page) => {
+			mergedPdf.addPage(page)
+		})
 	}
 
-	export function page(data: Content | { content: string | VNode<{}> }) {
-		if (P.isObject(data.content)) {
-			return {
-				...data,
-				content: renderToString(
-					data.content,
-					{},
-					{
-						pretty: true,
-					}
-				),
-			}
-		}
-		return data
-	}
+	const buffer = await mergedPdf.save()
+	return buffer
+}
 
-	async function mergePDF(pdfList: Buffer[]) {
-		const pdfToMerge = [...pdfList]
+async function singlePdf(content: Content) {
+	const format = P.isArray(content.format)
+		? {
+				...flipDimensions(content.format, true),
+		  }
+		: {
+				format: content.format,
+		  }
+	const result = await P.canFail(
+		() =>
+			html_to_pdf.generatePdf(
+				{
+					content: content.content,
+				},
+				{
+					...format,
+					landscape: content.landscape,
+				}
+			) as Promise<Buffer>
+	)
+	if (P.isError(result)) throw result
+	return result
+}
 
-		const mergedPdf = await PDFDocument.create()
-		for (const pdfBytes of pdfToMerge) {
-			const pdf = await PDFDocument.load(pdfBytes)
-			const copiedPages = await mergedPdf.copyPages(pdf, pdf.getPageIndices())
-			copiedPages.forEach((page) => {
-				mergedPdf.addPage(page)
-			})
-		}
-
-		const buffer = await mergedPdf.save()
-		return buffer
-	}
-
-	async function singlePdf(content: Content) {
-		const format = P.isArray(content.format)
-			? {
-					...flipDimensions(content.format, true),
-			  }
-			: {
-					format: content.format,
-			  }
-		const result = await P.canFail(
-			() =>
-				html_to_pdf.generatePdf(
-					{
-						content: content.content,
-					},
-					{
-						...format,
-						landscape: content.landscape,
-					}
-				) as Promise<Buffer>
-		)
-		if (P.isError(result)) throw result
-		return result
-	}
-
-	/**
-	 *
-	 * @returns {string} File to generated pdf file
-	 */
-	export async function generate(args: Content[]) {
-		const sample = await Promise.all(args.map((c) => singlePdf(c)))
-		const mergedPDF = await mergePDF(sample)
-		const name = tmpNameSync()
-		await Jet.writeAsync(name, mergedPDF)
-		return name
-	}
+/**
+ *
+ * @returns {string} File to generated pdf file
+ */
+export async function generate(args: Content[]) {
+	const sample = await Promise.all(args.map((c) => singlePdf(c)))
+	const mergedPDF = await mergePDF(sample)
+	const name = tmpNameSync()
+	await Jet.writeAsync(name, mergedPDF)
+	return name
 }
